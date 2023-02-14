ccecc8888a4d9e0c56a1c130eee5e39f30212941
Aleksandr Semeikin <sensem@yandex.ru>
2023-02-14 13:04:39 +0300

    Lesson 21 CRUD, REST, DAO

---
+++ src/main/java/ru/alexsem/springcourse/dao/PersonDAO.java
@@ -0,0 +1,33 @@
+package ru.alexsem.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ru.alexsem.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+    
+    {
+        people = new ArrayList<>();
+        people.add((new Person(++PEOPLE_COUNT, "Tom")));
+        people.add((new Person(++PEOPLE_COUNT, "Bob")));
+        people.add((new Person(++PEOPLE_COUNT, "Mike")));
+        people.add((new Person(++PEOPLE_COUNT, "Kate")));
+    }
+    
+    public List<Person> index() {
+        return people;
+    }
+    
+    public Person show(int id) {
+        return people.stream()
+                     .filter(person -> person.getId() == id)
+                     .findAny()
+                     .orElse(null);
+    }
+}
