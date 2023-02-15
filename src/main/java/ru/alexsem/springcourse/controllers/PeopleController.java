73773975e57dd02966088e81f707c538d43d03fe
Aleksandr Semeikin <sensem@yandex.ru>
2023-02-15 18:43:30 +0300

    Lesson 22 CREATE

--- src/main/java/ru/alexsem/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/alexsem/springcourse/controllers/PeopleController.java
@@ -3,9 +3,7 @@ package ru.alexsem.springcourse.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.alexsem.springcourse.dao.PersonDAO;
 import ru.alexsem.springcourse.models.Person;
 
@@ -27,7 +25,7 @@ public class PeopleController {
     public String index(Model model) {
 //        Получим всех людей из DAO и передадим на отображение в представление
         List<Person> people = personDAO.index();
-        model.addAttribute("people",people);
+        model.addAttribute("people", people);
         return "people/index";
     }
     
@@ -35,7 +33,33 @@ public class PeopleController {
     public String show(@PathVariable("id") int id, Model model) {
 //        Получим одного человека из DAO и передадим на отображение в представление
         Person person = personDAO.show(id);
-        model.addAttribute("person",person);
+        model.addAttribute("person", person);
         return "people/show";
+    }
+    
+    //    Метод создаёт Person, добавляет объект в модель,
+    //    возвращает html-форму. Далее поля инициализируются данными из формы
+//    и передаются в метод create() по адресу POST /people
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+//        альтернативный вариант создания объекта Person:
+//         model.addAttribute("person", new Person);
+        return "people/new";
+    }
+    
+    //   Метод принимает на вход post-запрос,
+//   берёт данные из этого post-запроса
+// и добавляет нового человека в базу данных с помощью DAO
+    @PostMapping()
+//  В модель будет положен новый объект Person со значениями полей из формы.
+//  Аннотация @ModelAttribute ДЛЯ POST-ЗАПРОСА сама создаёт объект с вставленными в
+//  форму полями и кладёт его в модель.
+//  Альтернативный вариант:
+//  Данные полей передаются в теле Post-request в формате name=Имя по адресу action="/people"
+//  и далее извлекаются через @RequestParam и создаётся Person. См на салйде
+//  CRUD_App2.
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
     }
 }
